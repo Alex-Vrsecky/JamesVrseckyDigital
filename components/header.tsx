f7ec45b0746434b3d5681453f62cca8a8d2f44bd
Alex Vrsecky <vrsecky102@gmail.com>
2025-12-18 21:56:37 +1100

    Higher quality logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
-import Logo from "../public/Logo.png";
+import Logo from "../public/Logo.svg";
 
 export default function Header() {
   return (
